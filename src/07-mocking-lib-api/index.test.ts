c41093b10d9f5c1496843db04f33d2cbcf28cfd2
whowouldwin <leranetwork@gmail.com>
2024-10-22 01:16:49 +0300

    feat: mocking library API

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,58 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+jest.mock('lodash', () => ({
+  throttle: jest.fn((fn) => fn),
+}));
 
 describe('throttledGetDataFromApi', () => {
+  const mockApiResponseData = {
+    id: 42,
+    title: 'test title',
+    content: 'test content',
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    (axios.create as jest.Mock) = jest.fn(() => ({
+      get: jest.fn().mockResolvedValue({ data: mockApiResponseData }),
+    }));
+
+    const response = await throttledGetDataFromApi('/users/42');
+
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
+
+    expect(response).toEqual(mockApiResponseData);
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockAxiosGet = jest
+      .fn()
+      .mockResolvedValue({ data: mockApiResponseData });
+    (axios.create as jest.Mock) = jest.fn(() => ({
+      get: mockAxiosGet,
+    }));
+
+    const relativeApiPath = '/users/42';
+    await throttledGetDataFromApi(relativeApiPath);
+    expect(mockAxiosGet).toHaveBeenCalledWith(relativeApiPath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockAxiosGet = jest
+      .fn()
+      .mockResolvedValue({ data: mockApiResponseData });
+    (axios.create as jest.Mock) = jest.fn(() => ({
+      get: mockAxiosGet,
+    }));
+
+    const response = await throttledGetDataFromApi('/users/42');
+    expect(response).toEqual(mockApiResponseData);
   });
 });
